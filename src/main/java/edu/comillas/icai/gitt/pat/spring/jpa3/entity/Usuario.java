8d6c23e317e0a6165944e4f8b055825412b93406
Juan David López Tapasco <j.lopez18@utp.edu.co>
2026-03-18 14:03:06 +0100

    Terminacion de las clases de entidad con persistencia

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/jpa3/entity/Usuario.java
@@ -0,0 +1,37 @@
+package edu.comillas.icai.gitt.pat.spring.jpa3.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    public String nombre;
+
+    @Column(nullable = false)
+    public String apellidos;
+
+    @Column(unique = true, nullable = false)
+    public String email;
+
+    @Column(nullable = false)
+    public String password;
+
+    @Column(nullable = false)
+    public String telefono;
+
+    @ManyToOne
+    @JoinColumn(name = "id_rol", nullable = false)
+    public Rol rol;
+
+    @Column(nullable = false)
+    public Date fechaRegistro;
+
+    @Column(nullable = false)
+    public Boolean activo;
+}
